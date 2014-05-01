8b30a41a89c5cd89a2b0201c0b35c97e1156834f
Koji Shiraishi <shiraco@gmail.com>
2014-05-01 13:43:43 +0900

    Remove Chef dir

--- .chef/knife.rb
+++
@@ -1,8 +0,0 @@
-log_level                :info
-log_location             STDOUT
-node_name                'shiraishi'
-client_key               '/Users/shiraishi/.chef/shiraishi.pem'
-validation_client_name   'chef-validator'
-validation_key           '/etc/chef-server/chef-validator.pem'
-chef_server_url          'https://Moon.local:443'
-syntax_check_cache_path  '/Users/shiraishi/.chef/syntax_check_cache'
